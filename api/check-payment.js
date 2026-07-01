04cbafe7b38aa00cd47f0add88a324fd8ef2b8dc
bizmedia1 <victorisrael539@gmail.com>
2026-07-01 03:47:53 +0100

    Update check-payment.js

--- api/check-payment.js
+++ api/check-payment.js
@@ -1,19 +1,29 @@
-import { payments } from "./store";
+import { SUPABASE_URL, SUPABASE_KEY } from "./store";
 
 export default async function handler(req, res) {
 
   const { account_number } = req.query;
 
-  if (!payments[account_number]) {
+  const response = await fetch(
+    `${SUPABASE_URL}/rest/v1/payments?account_number=eq.${account_number}&select=status`,
+    {
+      headers: {
+        apikey: SUPABASE_KEY,
+        Authorization: `Bearer ${SUPABASE_KEY}`
+      }
+    }
+  );
 
+  const data = await response.json();
+
+  if (!data.length) {
     return res.status(404).json({
       paid: false
     });
-
   }
 
   return res.status(200).json({
-    paid: payments[account_number].status === "paid"
+    paid: data[0].status === "paid"
   });
 
 }
